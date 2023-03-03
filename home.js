db4d0993006e2ca51ef55be0a0b2f4f0bc496394
Jones <Jonessajan3000@gmail.com>
2023-03-03 11:41:26 +0530

    sending message enabled

--- home.js
+++ home.js
@@ -1,19 +1,59 @@
-var form = document.getElementById("addForm");
 
-window.addEventListener("load", showUsers);
+// window.addEventListener("load", showUsers);
 
-async function showUsers() {
-  try {
+// async function showUsers() {
+//   try {
 
-    const response = await axios.get("http://localhost:3000/home/users", {
-      headers: { Authorization: localStorage.getItem("token") },
-    });
+//     const response = await axios.get("http://localhost:3000/home/users", {
+//       headers: { Authorization: localStorage.getItem("token") },
+//     });
 
-    console.log(response.data);
+//     console.log(response.data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+var message = document.getElementById("message");
+var send = document.getElementById("send");
+send.addEventListener("click", sendMessage);
+
+async function sendMessage() {
+  
+  try { 
+    console.log('sendMessage called')
+    console.log("///////////////////////////////",message.value)
+    var newItem1 = message.value
+    const data = {
+      message: newItem1,
+    };
+
+
+    const response = await axios.post(
+      "http://localhost:3000/chat/sendmessage",
+      data,
+      { headers: { Authorization: localStorage.getItem("token") } }
+    );
+    console.log(response)
+
+
+
+
+
+    message.value = "";
+
+
   } catch (error) {
-    console.error(error);
+    console.error("catch block called");    
+    console.log(error);    
+
+
+
+
   }
 }
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 
